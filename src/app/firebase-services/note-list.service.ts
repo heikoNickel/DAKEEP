00e4181d9833a4610738e11b0c461a0d466326f3
Heiko Nickel <heiko.nickel@gmx.net>
2024-09-23 22:31:47 +0200

    DAKEEP ready!

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
-import { collectionData, Firestore, collection, doc, onSnapshot, addDoc, updateDoc, deleteDoc } from '@angular/fire/firestore';
+import { query, orderBy, limit, where, collectionData, Firestore, collection, doc, onSnapshot, addDoc, updateDoc, deleteDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,14 +12,17 @@ export class NoteListService {
 
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
+  normalMarkedNotes: Note[] = [];
 
   unsubTrash;
   unsubNotes;
+  unsubMarkedNotes;
 
   firestore: Firestore = inject(Firestore);
 
   constructor() {
     this.unsubNotes = this.subNotesList();
+    this.unsubMarkedNotes = this.subMarkedNotesList();
     this.unsubTrash = this.subTrashList();
   }
 
@@ -85,6 +88,7 @@ export class NoteListService {
   ngonDestroy(){
     this.unsubNotes();
     this.unsubTrash();
+    this.unsubMarkedNotes();
   }
 
 
@@ -98,13 +102,25 @@ export class NoteListService {
   }
 
   subNotesList(){
-    return onSnapshot(this.getNotesRef(), (list) => {
+    const q = query(this.getNotesRef(), limit(100));
+    return onSnapshot(q, (list) => {
       this.normalNotes = [];
       list.forEach(element => {
         this.normalNotes.push(this.setNoteObject(element.data(), element.id));
       });
     });
   }
+
+  subMarkedNotesList(){
+    const q = query(this.getNotesRef(), where("marked", "==", true), limit(100));
+    return onSnapshot(q, (list) => {
+      this.normalMarkedNotes = [];
+      list.forEach(element => {
+        this.normalMarkedNotes.push(this.setNoteObject(element.data(), element.id));
+      });
+    });
+  }
+
 
   getNotesRef(){
     return collection(this.firestore, 'notes');
--- src/app/note-list/note-list.component.ts
+++ src/app/note-list/note-list.component.ts
@@ -24,7 +24,11 @@ export class NoteListComponent {
 
   getList(): Note[]{
     if (this.status == 'notes'){
-      return this.noteService.normalNotes;
+      if(this.favFilter == "all"){
+        return this.noteService.normalNotes;
+      } else {
+        return this.noteService.normalMarkedNotes;
+      }
     } else {
       return this.noteService.trashNotes;
     }
